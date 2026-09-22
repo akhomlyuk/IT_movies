a820f5133df6ca9372659638f399107837efac39
akhomlyuk <relicbs@gmail.com>
2026-09-22 20:49:29 +0300

    Replace langToEn/langToRu with single swapLang key; skip Metrika on GPC/DNT

--- tools/gen_pages.py
+++ tools/gen_pages.py
@@ -67,14 +67,17 @@ METRIKA_MARK = ("<!-- begin:metrika -->", "<!-- end:metrika -->")
 METRIKA_ID = "112571181"
 METRIKA_SCRIPT = """  <!-- Yandex.Metrika counter -->
   <script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id={id}', 'ym');
+    (function () {
+      if (navigator.globalPrivacyControl === true || navigator.doNotTrack === "1") return;
+      (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id={id}', 'ym');
 
-    ym({id}, 'init', {ssr:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+      ym({id}, 'init', {ssr:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+    })();
   </script>
   <noscript><div><img src="https://mc.yandex.ru/watch/{id}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
   <!-- /Yandex.Metrika counter -->""".replace("{id}", METRIKA_ID)
